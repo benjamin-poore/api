993576714331850296c1bc91a24f05a50aafae90
ben poore <bp@bens-MacBook-Pro.local>
2022-11-01 10:59:05 -0700

    fix linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator Functions
 """
 
-def add(x,y):
-  return x + y
 
-def subtract(x,y):
-  """subtract x from y and return result"""
-  return y -x
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x from y and return result"""
+    return y - x
